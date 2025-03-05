b52be3eff8808654255cb83da02d9dfac528928b
SoyAlexYC <157544784+SoyAlexYC@users.noreply.github.com>
2025-03-04 22:53:37 -0600

    update P2_ADA.c
    added tabs

--- P2_ADA.c
+++ P2_ADA.c
@@ -4,45 +4,45 @@ float dividend;
 float divisor;
 int res = 0;
 do{
-printf("Hola, ingrese el dividendo: \n");
-scanf("%f",&dividend);
-printf("Ingrese el divisor: \n");
-scanf("%f",&divisor);
+  printf("Hola, ingrese el dividendo: \n");
+  scanf("%f",&dividend);
+  printf("Ingrese el divisor: \n");
+  scanf("%f",&divisor);
 }while(divisor==0);
 float temp, temp2;
 temp = dividend;
 if((temp < 0) ^ (divisor < 0)){
-if(temp<0){
-temp2 = temp+divisor;
-while(temp2<=0){
-temp= temp + divisor;
-res-- ;
-temp2 = temp+divisor;
-}
-}
-else{
-temp2 = temp+divisor;
-while(temp2>=0){
-temp= temp + divisor;
-res-- ;
-temp2 = temp+divisor;
-}
-}
+  if(temp<0){
+    temp2 = temp+divisor;
+    while(temp2<=0){
+    temp= temp + divisor;
+    res-- ;
+    temp2 = temp+divisor;
+    }
+  }
+  else{
+  temp2 = temp+divisor;
+  while(temp2>=0){
+    temp= temp + divisor;
+    res-- ;
+    temp2 = temp+divisor;
+    }
+  }
 }else if(temp>0 && divisor > 0){
-while(temp>=divisor){
-temp= temp - divisor;
-res++;
-}
-}
-else{
-if(dividend == 0){
-res = 0;
+  while(temp>=divisor){
+    temp= temp - divisor;
+    res++;
+  }
 }
 else{
-while(temp<=divisor){
-temp= temp - divisor;
-res++;
-}
+  if(dividend == 0){
+    res = 0;
+  }
+else{
+  while(temp<=divisor){
+    temp= temp - divisor;
+    res++;
+  }
 }
 }
 printf("residuo: %.2f\n", temp);
